a82319c29627813d8539171927a003446ad2cb8b
Emirhan Şahin <sahnemirhan@gmail.com>
2021-12-18 17:57:23 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,36 @@
+Vue.createApp({
+data(){
+    return{
+        counter : 0,
+        counter2 : 0,
+
+    }
+
+},
+methods:{
+
+},
+computed : {
+    getCounterResult(){
+        console.log('Counter 1 Çalıştı');
+      return this.counter > 5 ? 'Greater than 5' : 'Less than or equal to 5'
+    },
+    getCounter2Result(){
+        console.log('Counter 2 Çalıştı');
+        return this.counter2 > 5 ? 'Greater than 5' : 'Less than or equal to 5'
+      }
+},
+watch:{
+    counter(newValue,oldValue){
+        console.log('Counter',oldValue,'=>',newValue);
+    },
+    getCounterResult(newValue,oldValue){
+        console.log('Result',oldValue,'=>',newValue);
+    }
+
+}
+
+
+
+
+}).mount('#app')
